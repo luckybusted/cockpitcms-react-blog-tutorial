5a0f52ae11389b3baa8c5b9d337b7892ebe9cd71
Ivan Ushmorov <ivan.ushmorov@zeiss.com>
2020-04-26 16:47:25 +0200

    build the single pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Material UI style dependencies
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    singleton: {
+        color: theme.palette.common.darkerGrey,
+    },
+    sociallinks: {
+        listStyle: 'none',
+        display: 'flex',
+        padding: '0',
+        justifyContent: 'space-evenly',
+    },
+}));
+
 const About = () => {
-  return (
-    <Grid container spacing={3}>
-      <h2>ABOUT</h2>
-    </Grid>
-  );
+    let classes = useStyles();
+
+    const [data, setData] = useState({});
+
+    async function fetchApi() {
+        let response = await fetch(
+            'https://cockpit.ushmorov.de/backend/api/singletons/get/About'
+        );
+        let data = await response.json();
+
+        setData(data);
+    }
+
+    useEffect(() => {
+        fetchApi();
+    }, []);
+
+    const renderHTML = (rawHTML) =>
+        React.createElement('div', {
+            dangerouslySetInnerHTML: { __html: rawHTML },
+        });
+
+    return (
+        <div className={classes.singleton}>
+            <h1>{data.Title}</h1>
+            {renderHTML(data.content)}
+
+            {data.sociallinks && (
+                <ul className={classes.sociallinks}>
+                    {data.sociallinks.map((link) => (
+                        <li key={link.value.name}>
+                            <a href={link.value.link}>{link.value.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 };
 
 export default About;
--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Material UI style dependencies
-import Grid from '@material-ui/core/Grid';
+
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    singleton: {
+        color: theme.palette.common.darkerGrey,
+    },
+}));
+
 const Legal = () => {
-  return (
-    <Grid container spacing={3}>
-      <h2>Legal Page</h2>
-    </Grid>
-  );
+    let classes = useStyles();
+
+    const [data, setData] = useState({});
+
+    async function fetchApi() {
+        let response = await fetch(
+            'https://cockpit.ushmorov.de/backend/api/singletons/get/Legal'
+        );
+        let data = await response.json();
+
+        setData(data);
+    }
+
+    useEffect(() => {
+        fetchApi();
+    }, []);
+
+    const renderHTML = (rawHTML) =>
+        React.createElement('div', {
+            dangerouslySetInnerHTML: { __html: rawHTML },
+        });
+
+    return (
+        <div className={classes.singleton}>
+            <h1>{data.Title}</h1>
+            {renderHTML(data.content)}
+        </div>
+    );
 };
 
 export default Legal;
